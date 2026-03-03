51a9cf7d065b2229fe00eca46ee4cf78d3859412
Arif Mufti Tharsa <105224041@student.universitaspertamina.ac.id>
2026-03-03 14:19:04 +0700

    Initial commit

---
+++ src/ktm.java
@@ -0,0 +1,15 @@
+public class ktm {
+    public static void main(String[] args) {
+        String nama = "Arif Mufti Tharsa";
+        String nim = "105224041";           
+        String prodi = "Ilmu Komputer"; 
+
+        System.out.println("============================");
+        System.out.println("KARTU TANDA MAHASISWA");
+        System.out.println("Nama \t: " + nama);
+        System.out.println("NIM \t: " + nim);
+        System.out.println("Prodi \t: " + prodi);
+        
+        System.out.println("============================");
+    }
+}
